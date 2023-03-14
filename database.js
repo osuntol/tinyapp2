5cf2a324d9de6bf51368c1fa3bdf21b1cfde60e5
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-03-14 01:11:52 +0000

    database js file to hold URL and  Users database

---
+++ database.js
@@ -0,0 +1,31 @@
+
+//URL DATABSE
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+};
+
+//USER DATABASE
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "1234",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "asdf",
+  },
+};
+
+module.exports = {
+  urlDatabase,
+  users
+}
